c441378ab7484e4bc28a2f0fcfc29d7bf6145f2e
Nhat123a <nhatdz22222@gmail.com>
2023-10-23 12:21:14 +0700

    Update

---
+++ Lab1_9/Controllers/NhanvienController.cs
@@ -0,0 +1,152 @@
+﻿using Lab1_9.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1_9.Controllers
+{
+    public class NhanvienController : Controller
+    {
+        public static List<Nhanviencs> dsnv = new List<Nhanviencs>();
+        // GET: Nhanvien
+        public ActionResult Index()
+        {
+
+            return View(dsnv);
+        }
+
+        // GET: Nhanvien/Details/5
+        public ActionResult Details(int id)
+        {
+            Nhanviencs nv = dsnv.FirstOrDefault(x => x.Manv == id);
+            return View(nv);
+        }
+
+        // GET: Nhanvien/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Nhanviencs nv)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    dsnv.Add(nv);
+                    return RedirectToAction("Index");
+                }
+                return View(nv);
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Nhanvien/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Nhanviencs nv = dsnv.FirstOrDefault(x => x.Manv == id);
+            return View(nv);
+        }
+
+        // POST: Nhanvien/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Nhanviencs nv)
+        {
+            Nhanviencs nhanvien = dsnv.FirstOrDefault(x => x.Manv == id);
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    nhanvien.Hoten = nv.Hoten;
+                    nhanvien.Gioitinh = nv.Gioitinh;
+                    nhanvien.hSL = nv.hSL;
+                    nhanvien.Phone = nv.Phone;
+                    nhanvien.phong = nv.phong;
+                    nhanvien.ngasinh = nv.ngasinh;
+                    return RedirectToAction("Index");
+                }
+                return View(nv);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Nhanviencs nhanvien = dsnv.FirstOrDefault(x => x.Manv == id);
+
+            return View(nhanvien);
+        }
+
+        // POST: Nhanvien/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                Nhanviencs nhanvien = dsnv.FirstOrDefault(x => x.Manv == id);
+                if (nhanvien != null)
+                {
+                    dsnv.Remove(nhanvien);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Luongcaonhat()
+        {
+            
+            var item = dsnv.OrderByDescending(x => x.Luong).Take(2).ToList();
+            if (item.Count == 0)
+            {
+                ViewBag.message = "Danh sách trống ";
+            }
+            return View(item);
+        }
+        //Tìm kiếm theo giới tính 
+        public ActionResult Timkiem(string Gioitinh)
+        {
+            bool gioiTinhValue = Gioitinh == "Nam"; 
+            var item = dsnv.Where(x => x.Gioitinh == gioiTinhValue).ToList();
+
+            return View(item); 
+        }
+        //Theo tuổi
+        public ActionResult Timkiemtuoi()
+        {
+
+            var danhSachNhanVien = dsnv
+                .Where(x => (DateTime.Now - x.ngasinh).Days / 365 < 30).ToList();
+            return View(danhSachNhanVien);
+
+        }
+        //Theo phòng
+        public ActionResult Timkiemtheophong(string tenphong)
+        {
+            IEnumerable<Nhanviencs> item = null; 
+
+            if (Enum.TryParse<Phong>(tenphong, out Phong phongEnum))
+            {
+                item = dsnv.Where(x => x.phong == phongEnum).ToList();
+            }
+
+            return View(item);
+        }
+
+    }
+}
---
+++ Lab1_9/Models/Nhanviencs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab1_9.Models
+{
+    public class Nhanviencs
+    {
+        [Key]
+        public int Manv { get; set; }
+        [Required,MinLength(8,ErrorMessage ="Nhập họ tên gồm 8 kí tự ")]
+        [Display(Name ="Họ Tên")]
+        public string Hoten { get; set; }
+        [Display(Name = "Giới Tính")]
+        public bool Gioitinh { get; set; }
+        [Display(Name = "Ngày sinh")]
+        [DataType(DataType.Date , ErrorMessage ="Ngày sinh bắt buộc đúng định dạng ")]
+        public DateTime ngasinh { get; set; }
+        [Required(ErrorMessage = "Số điện thoại là bắt buộc")]
+        [Display(Name = "Số Điện Thoại")]
+        public int Phone { get; set; }
+        [Display(Name = "Lương")]
+        public float Luong { get { return hSL * 1800000; } }
+        [Required(ErrorMessage = "Hệ số lương là bắt buộc")]
+        [Display(Name = "Hệ số lương")]
+        public float hSL { get; set; }
+        [Display(Name = "Phòng")]
+        public Phong phong { get; set; }
+    }
+    public enum Phong
+    {
+        HA8,
+        HA9,
+        HA10,
+        
+    }
+}
